f576ddc92c87edd774cd08ad3245534d0fbe34db
jebbx-96 <jessi.betke@web.de>
2025-03-14 21:42:17 +0100

    Update water_tracker.py

--- water_tracker.py
+++ water_tracker.py
@@ -12,8 +12,9 @@ if "water_count" not in st.session_state:
     st.session_state.day_count = 0
     st.session_state.plant_stage = 0
 
-# 7 Plant growth stages (updated with daily progression)
+# 8 Plant growth stages (including Day 0 seed stage)
 plant_images = [
+    "images/stage0_seed.png", 
     "images/stage1_sprout.png", 
     "images/stage2_seedling.png", 
     "images/stage3_young_plant.png", 
@@ -32,23 +33,22 @@ def get_motivation():
     return ""
 
 def log_water():
-    if st.session_state.water_count < WATER_GOAL:
-        st.session_state.water_count += 1
-        st.session_state.xp += XP_PER_CUP
-        
-        if st.session_state.water_count == WATER_GOAL:
-            # Update plant growth dynamically per day
-            st.session_state.plant_stage = min(st.session_state.day_count, len(plant_images) - 1)
-            st.session_state.day_count += 1
-            
-            if st.session_state.day_count == DAYS_TO_FOREST:
-                st.session_state.message = "🌳 You've grown a full forest! Resetting..."
-                reset_week()
-            else:
-                st.session_state.message = get_motivation()
+    st.session_state.water_count += 1
+    st.session_state.xp += XP_PER_CUP
     
-        time.sleep(0.2)
-        st.rerun()
+    if st.session_state.water_count == WATER_GOAL:
+        # Update plant growth dynamically per day
+        st.session_state.plant_stage = min(st.session_state.day_count + 1, len(plant_images) - 1)
+        st.session_state.day_count += 1
+        
+        if st.session_state.day_count == DAYS_TO_FOREST:
+            st.session_state.message = "🌳 You've grown a full forest! Resetting..."
+            reset_week()
+        else:
+            st.session_state.message = get_motivation()
+    
+    time.sleep(0.2)
+    st.rerun()
 
 def reset_day():
     st.session_state.water_count = 0
@@ -83,5 +83,4 @@ with col1:
         reset_day()
 
 with col2:
-    st.image(plant_images[st.session_state.plant_stage], caption="Your Growing Forest", use_column_width=True)
-
+    st.image(plant_images[st.session_state.plant_stage], caption="Your Growing Forest", use_container_width=True)
